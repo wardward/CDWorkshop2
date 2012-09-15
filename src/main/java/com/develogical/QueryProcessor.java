cb98a303a88f1d244427654ab8b4bfdb41a0b1bb
Ward Bergmans <ward.bergmans@b-it.nu>
2012-09-15 13:58:28 +0200

    a change

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,6 +3,7 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
+    	System.out.println("QueryProcessor.process() Query: " + query);
         if (query.contains("SPA2012")) {
             return "SPA is a conference";
         }
